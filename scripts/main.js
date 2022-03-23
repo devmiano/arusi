5b4f609eabfc2c95da559013dcb850dfb64ef0fd
Miano <dev.miano@gmail.com>
2022-03-23 19:36:02 +0300

    Merged modal and added image assets

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,28 @@
 'use strict';
 
-$(document).ready(function(){
-    // When the cart button is clicked
-    $('.cart').click(function(){
-        $('.overlay').toggle();
-        $('.cart-modal').slideDown();
-    })
+$(document).ready(function () {
+  // When the cart button is clicked
+  $('.cart').click(function () {
+    $('.overlay').toggle();
+    $('.cart-modal').slideDown();
+  });
 
-    // When overlay is clicked
-    $('.overlay').click(function(){
-        $('.overlay').toggle('hide');
-        $('.cart-modal').toggle('hide');
-    })
+  // When overlay is clicked
+  $('.overlay').click(function () {
+    $('.overlay').toggle('hide');
+    $('.cart-modal').toggle('hide');
+  });
 
-    // When the close modal button is clicked
-    $('.close-btn').click(function(){
-        $('.overlay').toggle('hide');
-        $('.cart-modal').toggle('hide');
-    })
+  // When the close modal button is clicked
+  $('.close-btn').click(function () {
+    $('.overlay').toggle('hide');
+    $('.cart-modal').toggle('hide');
+  });
 
-    // When a user clicks on the amount button
-    $('.modal-item-btn').click(function(){
-        $(this).css('backgroundColor', '#4e854e');
-        $(this).text("Added");
-        console.log(this);
-    })
-})
+  // When a user clicks on the amount button
+  $('.modal-item-btn').click(function () {
+    $(this).css('backgroundColor', '#4e854e');
+    $(this).text('Added');
+    console.log(this);
+  });
+});

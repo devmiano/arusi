fe33bb7ffbc998f4cc339bb33125e2c350a733dc
brian <otienolubalo@yahoo.com>
2022-03-25 01:06:18 +0300

    adding button fuctionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 $(document).ready(function () {
+    let totalItems = 0
+    let cost = 0
 
     // When the Jewel button is clicked
     $('.jewel-btn').click(function () {
@@ -79,9 +81,32 @@ $(document).ready(function () {
     $('.modal-item-btn').click(function () {
         $(this).css('backgroundColor', '#4e854e');
         $(this).text("Added");
-        console.log(this);
+        cost += parseInt($(this).attr('value'))
+        $('.fw-order-btn').html("Items selected: " + ++totalItems + '<br/>' + "total cost: " + cost);
+        
     })
+    });
+   // let carts = document.querySelectorAll(".modal-item-btn");
+ 
+//for (let i = 0; i < carts.length; i++) {
+ //carts[i].addEventListener("click", () => {
+  //  cartNumbers();
+ //});
+//}
+  
+ //adding items to the cart
+//function cartNumbers() {
+    //let productNumbers = localStorage.getItem("cartNumbers");
+    //console.log(productNumbers);
+    //productNumbers = parseInt(productNumbers);
+ 
+   // if (productNumbers) {
+     //   localStorage.setItem("cartNumbers", productNumbers + 1);
+       // document.querySelector(".total-order-btn span").textContent =
+         //   productNumbers + 1;
+    //} else {
+      //  localStorage.setItem("cartNumbers", 1);
+        //document.querySelector(".total-order-btn span").textContent = 1;
+  //  }
 
-    
-});
-
+//};

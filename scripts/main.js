6f4ff0b1869547b0f9e8e49dc63d52b796d14725
[alroude] <[nimrod.chitayi@gmail.com]>
2022-03-24 23:24:25 +0300

    created and implemented the pop-up modal functionalities

--- scripts/main.js
+++ scripts/main.js
@@ -59,14 +59,29 @@ $(document).ready(function(){
     })
 
     // When the Total Order Button is clicked
+
+    const orderTotal = function(){
+        // let item = document.querySelectorAll('.modal-item-btn');
+        // let values = $(item).val();
+        // item.forEach(values => {
+        //     console.log(values);
+        // });
+        $('.modal-item-btn').click(function(){
+            console.log($(this).val());
+        })    
+    }
+
     $('.total-order-btn').click(function(){
       $('.overlay').toggle('hide');
       $('.cart-modal').toggle('hide');
+      console.log(orderTotal());
+      
     })
 
     $('.gowns-order-btn').click(function(){
       $('.overlay2').toggle('hide');
       $('.gowns-modal').toggle('hide');
+
     })
 
     $('.fw-order-btn').click(function(){
@@ -78,8 +93,60 @@ $(document).ready(function(){
     $('.modal-item-btn').click(function(){
         $(this).css('backgroundColor', '#4e854e');
         $(this).text("Added");
-        console.log(this);
+        
+        let carts = document.querySelectorAll(".modal-item-btn");
+        
+        for (let i = 0; i < carts.length; i++) {
+        carts[i].addEventListener("click", () => {
+            cartNumbers(carts);
+        });
+        }
+        
+        function cartNumbers(){
+
+            let amount = document.querySelectorAll(".modal-item-btn").value;
+            let productNumbers = localStorage.getItem("cartNumbers");
+            console.log(productNumbers);
+            productNumbers = parseInt(productNumbers);
+            
+            if( productNumbers){
+                localStorage.setItem("cartNumbers", productNumbers + 1);
+                document.querySelector(".total-order-btn span").textContent =
+                    productNumbers + 1;
+                document.querySelector(".cart-counter").textContent =
+                    productNumbers + 1;
+                $('.i').text($('.modal-item-title').text());
+                $('.amount').text($(parseInt(amount)));
+
+                $('.cart-empty').hide();
+                $('.order-counter').text(productNumbers + 1);
+            }
+            // else{
+            //     localStorage.setItem("cartNumbers", 1) ;
+            //     document.querySelector(".total-order-btn span").textContent = 1;
+            //     document.querySelector(".cart-counter").textContent = 1;
+            // }
+        }
+        
+    });
+
+    // When the Cart Icon is clicked
+    $('.shopping-cart-btn').click(function(){
+        $('.shop-cart-modal').slideToggle('medium');
     })
 
-    
+    // When the close cart icon is clicked
+    $('.close-cart').click(function(){
+        $('.shop-cart-modal').slideToggle('medium');
+    })   
+
+    // When Complete Order button is clicked
+    $('.complete-order').click(function(){
+        $('.complete-order').toggle('hide');
+        $('.cart-order-total').toggle('hide');
+        $('.cart-order-items').hide();
+        $('.cart-counter').text("0");
+        $('.alert').toggle('show');
+    })
 })
+
